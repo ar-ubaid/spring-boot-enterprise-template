68e903d281d0499a9f7d1bb8e0e0b6af13824713
ar-ubaid <ar_ubaid@yahoo.com>
2026-08-25 10:04:56 +0500

    test: refinements

--- src/test/java/io/github/arubaid/config/MailConfigLogModeTest.java
+++ src/test/java/io/github/arubaid/config/MailConfigLogModeTest.java
@@ -1,27 +1,21 @@
 package io.github.arubaid.config;
 
-import io.github.arubaid.Application;
 import io.github.arubaid.common.notification.LoggingMailSender;
 import io.github.arubaid.common.notification.MailSender;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(
-        classes = Application.class,
         properties = {
                 "app.mail.mode=log",
                 "app.mail.from=no-reply@example.com"
         }
 )
-@Import(MailConfigLogModeTest.TestMailConfig.class)
 class MailConfigLogModeTest {
 
     @Autowired
@@ -34,16 +28,5 @@ class MailConfigLogModeTest {
     void shouldUseLoggingMailSenderWhenConfigured() {
         assertThat(mailSender)
                 .isInstanceOf(LoggingMailSender.class);
-    }
-
-    @TestConfiguration
-    static class TestMailConfig {
-
-        @Bean
-        JavaMailSender javaMailSender() {
-            return org.mockito.Mockito.mock(
-                    JavaMailSender.class
-            );
-        }
     }
 }
